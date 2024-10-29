7040f988d840f334b729e7e71d72588ef2f13cfb
mohammadreza Razzaz <mohammadrezarzz1999@gmail.com>
2024-10-29 15:59:49 +0330

    feat(ui): add complete pagination component with customizable styles and page change handling

---
+++ src/ui/components/customePagination/index.tsx
@@ -0,0 +1,40 @@
+"use client"
+import React from "react"
+import ReactPaginate from "react-paginate"
+import styles from "./Pagination.module.css"
+
+interface PaginationProps {
+  pageCount: number
+  onPageChange: (selectedPage: number) => void
+}
+
+const PaginationComponent: React.FC<PaginationProps> = ({
+  pageCount,
+  onPageChange,
+}) => {
+  const handlePageClick = (data: { selected: number }) => {
+    const selectedPage = data.selected + 1
+    onPageChange(selectedPage)
+  }
+
+  return (
+    <ReactPaginate
+      previousLabel={"<"}
+      nextLabel={">"}
+      breakLabel={"..."}
+      pageCount={pageCount}
+      marginPagesDisplayed={2}
+      pageRangeDisplayed={3}
+      onPageChange={handlePageClick}
+      containerClassName={styles.pagination}
+      pageClassName={styles.page_item}
+      activeClassName={styles.active}
+      previousClassName={styles.page_item}
+      nextClassName={styles.page_item}
+      disabledClassName={styles.disabled}
+      breakClassName={styles.page_item}
+    />
+  )
+}
+
+export default PaginationComponent
--- src/ui/view/paginateLayout/index.tsx
+++ src/ui/view/paginateLayout/index.tsx
@@ -1,8 +1,17 @@
+"use client"
 import React from "react"
+import { CustomePagination } from "@/ui/components"
 
 interface IProps {}
 
 const PaginateLayout: React.FC<IProps> = () => {
-  return <div className='bg-blue-50 absolute'>ddsadasdsadsadsadsadsaddaas</div>
+  return (
+    <div className='bg-blue-50 absolute bottom-[7%] w-full'>
+      <CustomePagination
+        pageCount={10}
+        onPageChange={() => console.log("hello")}
+      />
+    </div>
+  )
 }
 export default PaginateLayout
