55a85e4ba47ff5f86f5d9748d807ab8eba28827e
mohammadreza Razzaz <mohammadrezarzz1999@gmail.com>
2024-11-11 18:09:28 +0330

    fix(ssr-realworld): remove any type registerAction

--- src/utils/actions/registerAction.ts
+++ src/utils/actions/registerAction.ts
@@ -1,27 +1,27 @@
 "use server"
 
+import { AxiosError } from "axios"
+
 import { registerSchema } from "@/utils/validations/FormSchema"
 
 import { BaseApireq } from "../interceptors"
-import axios from "axios"
-import { headers } from "next/headers"
 
 const registerAction = async (
   username: string,
   password: string,
-  email: string,
+  email: string
 ) => {
   try {
     await registerSchema.validate(
       { email, password, username },
-      { abortEarly: false },
+      { abortEarly: false }
     )
 
     const resBody = {
       user: {
-        email: email,
-        password: password,
-        username: username,
+        email,
+        password,
+        username,
       },
     }
 
@@ -30,12 +30,13 @@ const registerAction = async (
     if (res.status === 201) {
       return { status: 201, message: "registered successfully" }
     }
-  } catch (error: any) {
-    const errorMessageKey = Object.keys(error?.response.data.errors)[0]
-    const errorMessage = error?.response.data.errors[errorMessageKey][0]
-    if (error.status === 422) {
-      return { status: 422, message: `${errorMessageKey} ${errorMessage}` }
-    } else {
+  } catch (error) {
+    if (error instanceof AxiosError && error.response) {
+      const errorMessageKey = Object.keys(error?.response.data.errors)[0]
+      const errorMessage = error?.response.data.errors[errorMessageKey][0]
+      if (error.status === 422) {
+        return { status: 422, message: `${errorMessageKey} ${errorMessage}` }
+      }
       return {
         status: 400,
         message: '"Something went wrong, please try again later!',
